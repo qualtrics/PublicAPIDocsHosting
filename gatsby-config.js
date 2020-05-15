450cdbcafcd3ecd4b7c9b609e91200b9b24a1df9
Jacob Pomeranz <jacobp@qualtrics.com>
2020-05-15 13:34:33 -0700

    Add gh-pages deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/PublicAPIDocsHosting",
   siteMetadata: {
     title: `Gatsby Stoplight Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
