7b779c43e059dc8bb72ef3ec73531c07ba400ef1
Chris <chrisnlott@gmail.com>
2020-05-19 13:16:00 -0500

    feat: add stoplight search component

--- src/components/stoplight-page.tsx
+++ src/components/stoplight-page.tsx
@@ -1,48 +1,19 @@
 import '@stoplight/elements/styles/elements.css';
 import '../styles/stoplight.css';
 
-import { Page, Provider, TableOfContents } from '@stoplight/elements';
+import { Page, TableOfContents } from '@stoplight/elements';
 import { Docs } from '@stoplight/elements/components/Docs';
 import { TryIt } from '@stoplight/elements/components/TryIt';
-import { Link } from 'gatsby';
-import { dirname, join, resolve } from 'path';
+import { join } from 'path';
 import React from 'react';
+
+import StoplightProvider from './stoplight-provider';
 
 const StoplightPage = ({ projectSrn, nodeUri, path }: { projectSrn: string; nodeUri: string; path: string }) => {
   const srn = join(projectSrn, nodeUri);
 
   return (
-    <Provider
-      host="https://qualtrics.stoplight.io/api"
-      components={{
-        link: ({ node, children }, key) => {
-          // Render a custom link component
-          let url = node.url;
-
-          // Open external links in a new tab
-          if (/^(http|#)/.test(url)) {
-            return (
-              <a href={url} target="_blank" rel="noreferrer noopener">
-                {children}
-              </a>
-            );
-          }
-
-          if (/^\./.test(url)) {
-            // Resolve relative links
-            url = `/${path}${resolve(dirname(nodeUri), url)}`;
-          } else {
-            url = url.replace(projectSrn, `/${path}`);
-          }
-
-          return (
-            <Link key={key} className="reset" to={url} title={node.title}>
-              {children}
-            </Link>
-          );
-        },
-      }}
-    >
+    <StoplightProvider projectSrn={projectSrn} nodeUri={nodeUri} path={path}>
       <div className="flex h-full w-full">
         <TableOfContents srn={decodeURIComponent(srn)} />
 
@@ -60,7 +31,7 @@ const StoplightPage = ({ projectSrn, nodeUri, path }: { projectSrn: string; node
           }}
         />
       </div>
-    </Provider>
+    </StoplightProvider>
   );
 };
 
