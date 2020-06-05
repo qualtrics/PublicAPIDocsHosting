168a58be8f553d7343e4d8e881d4a6f9a4d2ecb3
Jacob Pomeranz <jacobp@qualtrics.com>
2020-06-05 12:01:13 -0700

    Update meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Qualtrics API Docs`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@stoplight`,
+    description: `The official Qualtrics API documentation.`,
+    author: `@qualtrics`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
