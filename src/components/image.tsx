f2ab73cf7754427e943a9418d30dc1aac87a60c8
Jacob Pomeranz <jacobp@qualtrics.com>
2020-06-18 13:58:38 -0700

    Disable blur on image load

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -19,7 +19,7 @@ const Image = () => {
       placeholderImage: file(relativePath: { eq: "QualtricsXM_WHITE.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+            ...GatsbyImageSharpFluid_noBase64
           }
         }
       }
