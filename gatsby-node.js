ef5c43211c1ee97ad65e2c439e2fe66ad3136499
Jacob Pomeranz <jacobp@qualtrics.com>
2020-06-30 16:04:48 -0700

    Fix SDKs routing issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,7 @@ exports.onCreatePage = async ({ page, actions }) => {
 
   // page.matchPath is a special key that's used for matching pages
   // only on the client.
-  const match = page.path.match(/^\/(instructions|guides|api-reference)/);
+  const match = page.path.match(/^\/(instructions|guides|api-reference|sdks)/);
   if (match) {
     page.matchPath = `${match[0]}/*`;
 
